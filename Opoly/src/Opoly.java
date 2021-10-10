2c70e24d35891f1048fda4df4dcfe32f7238eecb
Gabriel Jones <gjones2023@chadwickschool.org>
2021-10-10 13:18:54 -0700

    feat: Made new functions
    I want to save this different thing

--- Opoly/src/Opoly.java
+++ Opoly/src/Opoly.java
@@ -1,71 +1,79 @@
 import java.util.Random;
 
 public class Opoly {
-		private int boardSize;
-		private int seed;
-		private int score; 
-		private int roundsOfPlay;
-		private int position;
-		private Random random;
-		private int oldPosition;
+	private int boardSize;
+	private int score;
+	private int roundsOfPlay;
+	private int position;
+	private Random random;
+
+	public Opoly(int intBoardSize, int intSeed) {
+		boardSize = intBoardSize;
+		random = new Random(intSeed);
+	}
+
+	public Opoly(int intBoardSize) {
+		boardSize = intBoardSize;
+		random = new Random();
+	}
+
+	public void playGame() {
+		while (gameOver() == false) {
+			move();
+
+			displayGame();
+		}
+
+	}
+
+	private boolean gameOver() {
+		if (score >= 1000) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	private void displayGame() {
+		for (int i = 0; i < boardSize; i++) {
+			if (position == i) {
+				System.out.print("o");
+			} else {
+				System.out.print("*");
+			}
+		}
+		System.out.println(" " + score +" "+position);
+	} 
+
+	private void move() {
+		position = position + getRandomNumber();
+		positionReset();
+		luckySevens();
+		endOfBoardRule();
+	}
+	private int getRandomNumber() {
+		return random.nextInt(5) + 1;
+	}
+	private void positionReset() {
+		if (position >= boardSize) {
+			position = position % boardSize;
+			score = score + 100;
+		}
+	}
+
+	private void luckySevens() {
+		if (position % 7 == 0) {
+			score = score * 2;
+		}
+	}
+
 	
-		public Opoly(int intBoardSize, int intSeed) {
-			boardSize = intBoardSize;
-			random = new Random(intSeed);
+
+	private void endOfBoardRule() {
+		if (position == boardSize-1) {
+			position = position - 3;
+			luckySevens();
 		}
-		public Opoly(int intBoardSize) {
-			boardSize = intBoardSize; 
-			random = new Random();
-		}
-		public void playGame() {
-			while(gameOver() == false) {
-				displayGame();
-				scoring();
-				move();
-				}
-				
-			}
-		public boolean gameOver() {
-			if(score < 1000) {
-				return false;
-				}
-			else {
-				return true;
-			}
-		}
-		public void displayGame(){
-			for(int i = 0; i < boardSize; i++) {
-				if(i == position%boardSize) {
-					System.out.print("o");
-				}
-				else {
-					System.out.print("*");
-				}	
-		}
-			System.out.println(" " + score);
-		}
-		public void move() {
-			position = oldPosition;
-			position = position + random.nextInt(5)+1;
-		}
-		public void scoring() {
-			
-			if(position%boardSize == boardSize) {
-				position = position - 3;
-				luckySevens();
-				}
-			else {
-				luckySevens();
-			}
-			if(position%boardSize <= oldPosition%boardSize) {
-				score = score + 100;
-			}
-		}
-			
-		
-		public void luckySevens() {
-			if(position % 7 == 0) {
-				score = score*2;
-			}
-		}
+	}
+
 }
